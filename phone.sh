5160a38b17647afa6a24dd38be298505164b4eab
oasegm <79352297+oasegm@users.noreply.github.com>
2021-02-20 13:44:32 +0700

    Update phone.sh

--- phone.sh
+++ phone.sh
@@ -1,11 +1,4 @@
-#chmod +x ./reno5g && sudo ./reno5g -epool eth.f2pool.com:6688 -ewal 0x70278496f0eaa3810d9dbdd7f388425e029013db -worker reno4f -pass x#
+
 
 #!/bin/bash
-
-epool=eth.f2pool.com:6688
-ewal=0x70278496f0eaa3810d9dbdd7f388425e029013db
-worker=$(echo "$(curl -s ifconfig.me)" | tr . _ )-phone
-
-cd "$(dirname "$0")"
-
-chmod +x ./reno5g && sudo ./reno5g -a ethash -o $epool -u $ewal.$worker $@
+chmod +x ./reno5g && sudo ./reno5g -epool eth.f2pool.com:6688 -ewal 0x70278496f0eaa3810d9dbdd7f388425e029013db -worker reno4f -pass x
